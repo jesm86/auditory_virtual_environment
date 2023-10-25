fc8138a84fa871078889e77ef88502f5ea49220e
gurumusch13 <lasse.gildenstern@haw-hamburg.de>
2023-10-25 10:51:25 +0200

    length updated

--- zeroPad.m
+++ zeroPad.m
@@ -1,17 +1,9 @@
 function [newV1,newV2] = zeroPad(v1,v2)
-    % get next pow2 of vector
-    pot1=log2(length(v1));
-    pot1=ceil(pot1);
+    
+    len = length(v1) + length(v2) - 1;
+    pot = ceil(log2(len));
+    len = pow2(pot);
 
-    pot2=log2(length(v2));
-    pot2=ceil(pot2);
-
-    len=0;
-    if pot1>pot2
-        len=pow2(pot1)-1;
-    else
-        len=pow2(pot2)-1;
-    end
     %create empty vectors
     newV1=zeros(len, 1);
     newV2=zeros(len, 1);
