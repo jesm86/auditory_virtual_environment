985637d2a68b4f9e2b77c85f76c5b888ebe99897
gurumusch13 <lasse.gildenstern@haw-hamburg.de>
2023-12-13 16:43:48 +0100

    fixed breakfunction

--- realTimeConvAndOutput.m
+++ realTimeConvAndOutput.m
@@ -2,27 +2,19 @@ function [] = realTimeConvAndOutput(h,Fs,blockSize,breakFunc)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+% set convolution blocksize
 convBlocksize=blockSize+length(h)-1;
 
-% recorder = audioDeviceReader(Fs, blockSize);
-% player = audioDeviceWriter(Fs);
-
+% initialize playerRecorder object
 playRec = audioPlayerRecorder(Fs);
 
+% preallocate overlap
 audioOverlap = zeros(convBlocksize,1);
 
+% preallocate output
 output = zeros(blockSize,1);
 
-h_fig = figure;
-set(h_fig,'KeyPressFcn',@myfun);
-
-kbhit = false;
-
-function myfun(~, event)
-    kbhit = true;
-end
-
-while ~kbhit
+while ~breakFunc()
     % read a block of audio data and output the one processed before
     data = playRec(output);
 
@@ -32,8 +24,9 @@ while ~kbhit
     % Perform fast convolution on current block 
     overlapSaveReturn = fftConv(audioOverlap,h);     
     output = overlapSaveReturn(convBlocksize-blockSize+1:convBlocksize);
-
-    pause(0.01)
+    
+    % pause to allow parallel functions to work
+    pause(0.001)
 end
 release(playRec);
 end
