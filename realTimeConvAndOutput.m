d2ad3ba85d196bfd16c0598be919be098316c6eb
jesm86 <jerome@pop-os.localdomain>
2023-12-18 21:46:10 +0100

    fix some comments

--- realTimeConvAndOutput.m
+++ realTimeConvAndOutput.m
@@ -4,31 +4,13 @@ function [output, audioOverlap] = realTimeConvAndOutput(input, audioOverlap, h,b
 
 % % set convolution blocksize
  convBlocksize=blockSize+length(h)-1;
-% 
-% % initialize playerRecorder object
-% %playRec = audioPlayerRecorder(Fs);
-% 
-% % % preallocate overlap
-%  audioOverlap = zeros(convBlocksize,1);
-% 
-% % preallocate output
-% output = zeros(blockSize,2);
 
-%while ~breakFunc()
-    % read a block of audio data and output the one processed before
-    %data = playRec(output); !!
+audioOverlap = [audioOverlap(blockSize+1:end);input];
 
-    % shift old audioOverlap and append new data
-    audioOverlap = [audioOverlap(blockSize+1:end);input];
+% Perform fast convolution on current block 
+overlapSaveReturnLeft = fftConv(audioOverlap,h(:,1));
+overlapSaveReturnRight = fftConv(audioOverlap,h(:,1));%2));
+output(:,1)= overlapSaveReturnLeft(convBlocksize-blockSize+1:convBlocksize,:);
+output(:,2) = overlapSaveReturnRight(convBlocksize-blockSize+1:convBlocksize,:);
 
-    % Perform fast convolution on current block 
-    overlapSaveReturnLeft = fftConv(audioOverlap,h(:,1));
-    overlapSaveReturnRight = fftConv(audioOverlap,h(:,1));%2));
-    output(:,1)= overlapSaveReturnLeft(convBlocksize-blockSize+1:convBlocksize,:);
-    output(:,2) = overlapSaveReturnRight(convBlocksize-blockSize+1:convBlocksize,:);
-    
-    % pause to allow parallel functions to work
-   % pause(0.001)
-%end
-%release(playRec);
 end
